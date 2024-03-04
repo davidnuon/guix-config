562b000182b48c1baa736044d6fdc9ac9b38c595
David Nuon <davidnuongm@gmail.com>
2024-03-03 16:16:15 -0800

    First commit

---
+++ hosts/dn-guixvm/config.scm
@@ -0,0 +1,72 @@
+;; This is an operating system configuration generated
+;; by the graphical installer.
+;;
+;; Once installation is complete, you can learn and modify
+;; this file to tweak the system configuration, and pass it
+;; to the 'guix system reconfigure' command to effect your
+;; changes.
+
+
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu))
+(use-service-modules cups desktop networking ssh xorg)
+
+(operating-system
+  (locale "en_US.utf8")
+  (timezone "America/Los_Angeles")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "dn-guixvm")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "davidnuon")
+                  (comment "David Nuon")
+                  (group "users")
+                  (home-directory "/home/davidnuon")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append (map specification->package '(
+			"nss-certs"
+			"vim"
+			"git"
+		     )
+		      )
+                    %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service enlightenment-desktop-service-type)
+
+                 ;; To configure OpenSSH, pass an 'openssh-configuration'
+                 ;; record as a second argument to 'service' below.
+                 (service openssh-service-type)
+                 (service cups-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-bootloader)
+                (targets (list "/dev/sda"))
+                (keyboard-layout keyboard-layout)))
+  (swap-devices (list (swap-space
+                        (target (uuid
+                                 "de656450-3812-4990-9c12-95b981e1e7c1")))))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "a8653cad-973b-46e4-bd3b-2709ff4a1f5a"
+                                  'ext4))
+                         (type "ext4")) %base-file-systems)))
