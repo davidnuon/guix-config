a024cd87cf22cc0a281ceb657da88db54807ebe3
David Nuon <davidnuongm@gmail.com>
2024-03-13 09:33:11 -0700

    Add t480 config

---
+++ hosts/dn-guix-t480/config.scm
@@ -0,0 +1,65 @@
+;; Indicate which modules to import to access the variables
+;; used in this configuration.
+(use-modules (gnu))
+(use-service-modules cups desktop networking ssh xorg)
+(use-modules  (nongnu packages linux)
+             (nongnu system linux-initrd))
+
+(operating-system
+  (kernel linux)
+  (initrd microcode-initrd)
+  (firmware (list linux-firmware))
+  (locale "en_US.utf8")
+  (timezone "America/Los_Angeles")
+  (keyboard-layout (keyboard-layout "us"))
+  (host-name "dn-guix-t480")
+
+  ;; The list of user accounts ('root' is implicit).
+  (users (cons* (user-account
+                  (name "davidnuon")
+                  (comment "David Nuon")
+                  (group "users")
+                  (home-directory "/home/davidnuon")
+                  (supplementary-groups '("wheel" "netdev" "audio" "video")))
+                %base-user-accounts))
+
+  ;; Packages installed system-wide.  Users can also install packages
+  ;; under their own account: use 'guix search KEYWORD' to search
+  ;; for packages and 'guix install PACKAGE' to install a package.
+  (packages (append 
+		    (map specification->package '("vim" "nss-certs" "icecat" "git" "curl" "wget" "openssh"))
+                    %base-packages))
+
+  ;; Below is the list of system services.  To search for available
+  ;; services, run 'guix system search KEYWORD' in a terminal.
+  (services
+   (append (list (service gnome-desktop-service-type)
+                 (service cups-service-type)
+                 (set-xorg-configuration
+                  (xorg-configuration (keyboard-layout keyboard-layout))))
+
+           ;; This is the default list of services we
+           ;; are appending to.
+           %desktop-services))
+  (bootloader (bootloader-configuration
+                (bootloader grub-efi-bootloader)
+                (targets (list "/boot/efi"))
+                (keyboard-layout keyboard-layout)))
+  (swap-devices (list (swap-space
+                        (target (uuid
+                                 "f937ad37-ed9a-4aac-866f-6f6759b8ab71")))))
+
+  ;; The list of file systems that get "mounted".  The unique
+  ;; file system identifiers there ("UUIDs") can be obtained
+  ;; by running 'blkid' in a terminal.
+  (file-systems (cons* (file-system
+                         (mount-point "/boot/efi")
+                         (device (uuid "B9B3-57D1"
+                                       'fat32))
+                         (type "vfat"))
+                       (file-system
+                         (mount-point "/")
+                         (device (uuid
+                                  "920368b2-923e-4a87-a797-2049592ab0d3"
+                                  'ext4))
+                         (type "ext4")) %base-file-systems)))
